ea26b03151ea44f161a0ba679f4800cb08c947ed
pecancake <33271004+pecancake@users.noreply.github.com>
2020-03-15 01:20:42 -0400

    Test

--- __init__.py
+++ __init__.py
@@ -8,10 +8,7 @@ def draw_square(scope, x1, y1, x2, y2, col):
         for x in range(x1, x2+1):
             scope.draw_pixel((x, y), col)
 
-def col_test(scope, chunk):
-    """ Prerequsite: chunk can cleanly divide 320 and 480. """
-    # create a generator for pixel choices
-
+def col_test(scope):
     for h in range(0, 320+1):
         for s in [1]:
             for v in range(0, 480+1):
@@ -23,10 +20,11 @@ err = "No error!\n"
 try:  
     s = Scope()
     
-    col_test(s, 5)
+    col_test(s)
 
     s.update()
     time.sleep(3)
+    print(4/0)
     print("done")
 
 except Exception:
