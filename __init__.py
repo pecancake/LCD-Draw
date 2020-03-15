ba92b46bb391366f14566c83d9e282be557bf06e
pecancake <33271004+pecancake@users.noreply.github.com>
2020-03-14 23:46:27 -0400

    Fix

--- __init__.py
+++ __init__.py
@@ -1,10 +1,15 @@
 from scope import Scope
 
-s = Scope()
+try:
+    
+    s = Scope()
 
-for y in range(100):
-    for x in range(100):
-        s.draw_pixel((100+x, 100+y), (0, 255, 0))
+    for y in range(100):
+        for x in range(100):
+            s.draw_pixel((100+x, 100+y), (0, 255, 0))
 
-s.update()
-time.sleep(2)
+    s.update()
+    time.sleep(2)
+
+except Exception as e:
+    print(e)
