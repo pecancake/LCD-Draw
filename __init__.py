1b7fdadeb6bb0e050feff88062ea6148eecfb711
pecancake <33271004+pecancake@users.noreply.github.com>
2020-03-15 02:13:34 -0400

    Fix

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ def draw_text(scope, x, y, text):
                 for xi in range(5):
                     if ProtoFont[text[char].upper()][yi*5+xi] == "1":
                         f.write(f"writing {text[char]} to {x+char*5+xi}, {y+yi}")
-                        draw_square(scope, x+char*10+xi, y+yi, x+char*10+xi+2, y+yi+2, (0,0,0))
+                        draw_square(scope, x+char*10+xi, y+yi, x+char*10+xi+1, y+yi+1, (0,0,0))
 
         
 def draw_square(scope, x1, y1, x2, y2, col):
