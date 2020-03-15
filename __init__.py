01c302501a0151bd6ad6a120c3ddfedfe1138073
pecancake <33271004+pecancake@users.noreply.github.com>
2020-03-15 00:21:43 -0400

    Fix

--- __init__.py
+++ __init__.py
@@ -6,19 +6,23 @@ def draw_square(scope, x1, y1, x2, y2, col):
         for x in range(x1, x2+1):
             scope.draw_pixel((x, y), col)
 
-def col_test(scope):
+def col_test(scope, chunk):
+    """ Prerequsite: chunk can cleanly divide 320 and 480. """
     # create a generator for pixel choices
-    gen = ((x, y) for y in range(320) for x in range(480))
+    gen = ((x*chunk, y*chunk) for x in range(52) for y in range(26))
     
     for r in range(0, 255, 10):
         for g in range(0, 255, 10):
             for b in range(0, 255, 10):
-                scope.draw_pixel(next(gen), (r, g, b))
+                coor = next(gen)
+                draw_square(coor[0], coor[1],
+                            coor[0]+chunk, coor[1]+chunk,
+                            (r, g, b)
                 
 try:  
     s = Scope()
     
-    col_test(s)
+    col_test(s, 2)
 
     s.update()
     time.sleep(5)
