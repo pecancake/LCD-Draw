78013cb647256f57b3947e3b908cf3c008799453
pecancake <33271004+pecancake@users.noreply.github.com>
2020-03-15 01:09:19 -0400

    Fix

--- __init__.py
+++ __init__.py
@@ -11,20 +11,12 @@ def draw_square(scope, x1, y1, x2, y2, col):
 def col_test(scope, chunk):
     """ Prerequsite: chunk can cleanly divide 320 and 480. """
     # create a generator for pixel choices
-    gen = ((x*chunk, y*2*chunk) for x in range(52) for y in range(26))
-    
+
     for h in range(0, 320+1):
         for s in [1]:
             for v in range(0, 480+1):
-                try:
-                    coor = next(gen)
+                scope.draw_pixel(h, v, tuple(i*255 for i in colorsys.hsv_to_rgb(h/320, s/1, v/480))
 
-                except StopIteration:
-                    return
-                
-                draw_square(scope, coor[0], coor[1],
-                            coor[0]+chunk, coor[1]+chunk*2,
-                            tuple(i*255 for i in colorsys.hsv_to_rgb(h/320, s/1, v/480)))
 
 err = "No error!"
 
@@ -41,4 +33,5 @@ except Exception:
     err = traceback.format_exc()
 
 with open("error", "w") as f:
-    f.write(err)
+    f.write(f'\n{err}\n')
+    
